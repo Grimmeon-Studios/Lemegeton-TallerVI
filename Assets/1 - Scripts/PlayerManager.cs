4b025a375c94106be1c3c8d11217ee5b696e0f9c
JU4NP4260 <82065985+JU4NP4260@users.noreply.github.com>
2023-08-10 22:48:36 -0500

    Player Movement Working

--- Assets/1 - Scripts/PlayerManager.cs
+++ Assets/1 - Scripts/PlayerManager.cs
@@ -5,31 +5,47 @@ using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
-    public Vector2 MovementInput { get; set; }
+    [SerializeField] float _Speed = 3;
+    [SerializeField] Camera _Camera;
+    PlayerInput_map _Input;
+    Vector2 _Movement;
+    Vector2 _DampedSpeed;
 
-    [SerializeField]
-    private InputActionReference input_reference;
-    private float maxSpeed = 5f;
+    Rigidbody2D _Rigidbody;
 
-    private Rigidbody2D rigidB;
-    private float currentSpeed;
-
-         
-    void Start()
+    private void Awake()
     {
-        rigidB = GetComponent<Rigidbody2D>();
+        _Input = new PlayerInput_map();
+        _Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        _Input.Enable();
+
+        _Input.Player.Move.performed += OnMovement;
+        _Input.Player.Move.canceled += OnMovement;
+    }
+
+    private void OnDisable()
+    {
+        _Input.Player.Move.performed -= OnMovement;
+        _Input.Player.Move.canceled -= OnMovement;
+
+        _Input.Disable();
+    }
+
+    private void OnMovement(InputAction.CallbackContext context)
+    {
+        _Movement = context.ReadValue<Vector2>();
     }
 
     private void FixedUpdate()
     {
-        if(MovementInput.magnitude > 0 && currentSpeed >= 0)
-        {
-        
-        }
+        _DampedSpeed = Vector2.SmoothDamp(_DampedSpeed, _Movement, ref _DampedSpeed, 0.05f);
+
+        _Rigidbody.velocity = _DampedSpeed * _Speed;
+
     }
 
-    void Update()
-    {
-        MovementInput = input_reference.action.ReadValue<Vector2>();
-    }
 }
