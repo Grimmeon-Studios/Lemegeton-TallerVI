8d9a1adb016700254398ec53e0a59b4f9a6ca5af
JU4NP4260 <82065985+JU4NP4260@users.noreply.github.com>
2023-08-30 10:49:14 -0500

    Scene Tweaks (idk what I did)

---
+++ Assets/1 - Scripts/DungeonManager/DungeonManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonManager : MonoBehaviour
+{
+    private float currentTime;
+    private int difficultylvl;
+}
