15dfac62279f707104f2c307b49107ec5eebf3bd
JU4NP4260 <82065985+JU4NP4260@users.noreply.github.com>
2023-09-01 00:13:28 -0500

    Dungeon Manager Basic Functionality

--- Assets/1 - Scripts/DungeonManager/DungeonManager.cs
+++ Assets/1 - Scripts/DungeonManager/DungeonManager.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DungeonManager : MonoBehaviour
 {
     private float currentTime;
-    private int difficultylvl;
-    private bool floorClear;
+    private int difficultylvl = 5;
+    private bool circleCleared;
+    private bool PlayerAlive;
     
     private BoxCollider2D gameAreaCollider;
 
@@ -19,25 +21,60 @@ public class DungeonManager : MonoBehaviour
     // Private Members Encapsulation
     public float _currentTime { get => currentTime; set => currentTime = value; }
     public int _difficultylvl { get => difficultylvl; set => difficultylvl = value; }
-    public bool _floorClear { get => floorClear; set => floorClear = value; }
+    public bool _circleCleared { get => circleCleared; set => circleCleared = value; }
 
     private void Awake()
     {
         gameAreaCollider = GetComponent<BoxCollider2D>();
     }
 
-    private void Start()
+    private void Update()
     {
         
     }
 
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Room") && gameAreaCollider.bounds.Contains(other.transform.position))
+
+        if (other.gameObject.CompareTag("Room") && !roomsInsideCollider.Contains(other.gameObject))
         {
             roomsInsideCollider.Add(other.gameObject);
-            Debug.Log("Added " + other.gameObject.name + " to the list.");
+            Debug.Log("Added " + other.gameObject + " to the list.");
+        }
+        else if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerAlive = true;
+            Debug.Log("Player has been detected");
         }
     }
 
+    public void OnRoomCleared(GameObject currentRoom)
+    {
+        if(currentRoom.GetComponent<Room>() != null)
+        {
+            if (currentRoom.CompareTag("Room") && roomsInsideCollider.Contains(currentRoom))
+            {
+                roomsInsideCollider.Remove(currentRoom);
+
+                if (roomsInsideCollider.Count == 0)
+                {
+                    Debug.Log("DungeonCleared");
+                    circleCleared = true;
+                    OnCicleCleared();
+                }
+
+                Destroy(currentRoom);
+            }
+        }
+    }
+
+    public void OnCicleCleared()
+    {
+        if (circleCleared)
+        {
+            SceneManager.LoadScene(0);
+        }
+        
+    }
 }
--- Assets/1 - Scripts/RoomsManager/Room.cs
+++ Assets/1 - Scripts/RoomsManager/Room.cs
@@ -1,35 +1,100 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Room : MonoBehaviour
 {
-    private int enemies;
+    public UnityEvent NoRemainingEnemies;
 
     private EdgeCollider2D edgeCollider;
+    private GameObject dungeonManager;
+    private DungeonManager _dungeonManager;
+    private HashSet<GameObject> enemiesHashSet = new HashSet<GameObject>();
 
     // The first Collider is the one who detects the player and trapts it in the Room
     [SerializeField] private BoxCollider2D mainBoxCollider;
+    [SerializeField] private GameObject enemyPrefab;
 
 
     void Awake()
     {
         edgeCollider = GetComponent<EdgeCollider2D>();
         mainBoxCollider = GetComponent<BoxCollider2D>();
+        mainBoxCollider.enabled = true;
         edgeCollider.enabled = false;
     }
 
-    public void OnTriggerEnter2D(Collider2D @object)
+    private void Start()
     {
+        dungeonManager = GameObject.Find("Dungeon Manager");
+        _dungeonManager = dungeonManager.GetComponent<DungeonManager>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        var @object = other.gameObject;
         if (@object.CompareTag("Player"))
         {
             edgeCollider.enabled = true;
             Debug.Log("Player Entered");
+            mainBoxCollider.size = mainBoxCollider.size * 1.3f;
+            SpawnEnemies(_dungeonManager._difficultylvl);
         }
         else if(@object.CompareTag("Enemy"))
         {
-            enemies = +1;
+            if (!enemiesHashSet.Contains(@object))
+            {
+                enemiesHashSet.Add(@object);
+            }
         }
-        mainBoxCollider.enabled = false;
+        else if(!@object.CompareTag("Dungeon Manager"))
+        {
+            Debug.Log("Entity: "+ @object.name + " Not Recognized");
+        }
     }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        
+        var @object = other.gameObject;
+        if (@object.CompareTag("Player"))
+        {
+            Debug.Log("Player Exited the Room");
+        }
+        else if (@object.CompareTag("Enemy"))
+        {
+            if (enemiesHashSet.Contains(@object))
+            {
+                enemiesHashSet.Remove(@object);
+                Destroy(@object);
+                if(enemiesHashSet.Count == 0)
+                {
+                    NoRemainingEnemies.Invoke();
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Entity: " + @object.name + " Not Recognized");
+        }
+
+    }
+
+    private void RandomEnemySpawnPos()
+    {
+        //To Be implemented
+    }
+
+    private void SpawnEnemies(int dificulty)
+    {
+        int i;
+        for (i = 0; i < dificulty; i++)
+        {
+            Instantiate(enemyPrefab, mainBoxCollider.bounds.center, Quaternion.identity);
+        }
+    }
+
+
 }
