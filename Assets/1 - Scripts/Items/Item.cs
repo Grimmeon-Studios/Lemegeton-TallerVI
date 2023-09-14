d463484c4fc859f60341db3f1d664c98a51c5e06
Pipe2303 <piperoldanrestrepo@hotmail.com>
2023-09-14 17:46:26 -0500

    Basic Item SpriteList

--- Assets/1 - Scripts/Items/Item.cs
+++ Assets/1 - Scripts/Items/Item.cs
@@ -22,60 +22,88 @@ public class Item : MonoBehaviour
     public float item_criticalDamage;
     public float item_timeInvincible;
 
+    public List<Sprite> spriteTexturesList;
+    //public ItemsTextures textures;
+    public SpriteRenderer sprite;
+
+    private void Start()
+    {
+        spriteTexturesList = new List<Sprite>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
 
     void Awake()
     {
-        int switchIndex = UnityEngine.Random.Range(0, activeItemsNumber+1);
-
-        switch (switchIndex)
+        /*for (int i = 0; i < textures.spriteList.Length; i++)
         {
-            case 0: // Movement Speed
-                item_speed = 0.2f;
-                break;
+            spriteTexturesList[i] = textures.spriteList[i];
+        }*/
+        int switchIndex = UnityEngine.Random.Range(0, activeItemsNumber+1);
+        
+        if (spriteTexturesList.Count <= 12)
+        {
+            switch (switchIndex)
+            {
+                case 0: // Movement Speed
+                    item_speed = 0.2f;
+                    sprite.sprite = spriteTexturesList[0];
+                    break;
 
-            case 1: // Maximun Health
-                item_maxHealth = 0.5f;
-                break;
+                case 1: // Maximun Health
+                    item_maxHealth = 0.5f;
+                    sprite.sprite = spriteTexturesList[1];
+                    break;
 
-            case 2: // Health Regeneration
-                item_health = 1;
-                break;
+                case 2: // Health Regeneration
+                    item_health = 1;
+                    sprite.sprite = spriteTexturesList[2];
+                    break;
 
-            case 3: // Maximun Defence
-                item_maxDefense = 0.5f;
-                break;
+                case 3: // Maximun Defence
+                    item_maxDefense = 0.5f;
+                    sprite.sprite = spriteTexturesList[3];
+                    break;
 
-            case 4: // Defence Regeneration
-                item_defense = 5;
-                break;
+                case 4: // Defence Regeneration
+                    item_defense = 5;
+                    sprite.sprite = spriteTexturesList[4];
+                    break;
 
-            case 5: // Player Melee Damage
-                item_attack = 2;
-                break;
+                case 5: // Player Melee Damage
+                    item_attack = 2;
+                    sprite.sprite = spriteTexturesList[5];
+                    break;
 
-            case 6: // Player Shot Damage
-                item_shotDamage = 3;
-                break;
+                case 6: // Player Shot Damage
+                    item_shotDamage = 3;
+                    sprite.sprite = spriteTexturesList[6];
+                    break;
 
-            case 7: // Player Shot Speed
-                item_shotSpeed = 0.6f;
-                break;
+                case 7: // Player Shot Speed
+                    item_shotSpeed = 0.6f;
+                    sprite.sprite = spriteTexturesList[7];
+                    break;
 
-            case 8: // Player Shot Range
-                item_shotRange = 1.5f;
-                break;
+                case 8: // Player Shot Range
+                    item_shotRange = 1.5f;
+                    sprite.sprite = spriteTexturesList[8];
+                    break;
 
-            case 9: // Critical Damage Probability
-                item_criticalRateUp = 2;
-                break;
+                case 9: // Critical Damage Probability
+                    item_criticalRateUp = 2;
+                    sprite.sprite = spriteTexturesList[9];
+                    break;
 
-            case 10: // Critical Damage
-                item_criticalDamage = 3;
-                break;
+                case 10: // Critical Damage
+                    item_criticalDamage = 3;
+                    sprite.sprite = spriteTexturesList[10];
+                    break;
 
-            case 11: // Invencibility Time after being hit
-                item_timeInvincible = 0.2f;
-                break;
+                case 11: // Invencibility Time after being hit
+                    item_timeInvincible = 0.2f;
+                    sprite.sprite = spriteTexturesList[11];
+                    break;
+            }
         }
     }
 
---
+++ Assets/1 - Scripts/Items/ItemsTextures.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "itemTextures", menuName = "ScriptableObjects/Item/ItemsTextures", order = 0)] 
+public class ItemsTextures: ScriptableObject
+{
+    public Sprite[] spriteList;
+    
+}
