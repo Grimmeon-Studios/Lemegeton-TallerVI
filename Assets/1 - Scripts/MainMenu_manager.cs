58774b547a5d900e7ce244354a5271ba8befa580
JU4NP4260 <82065985+JU4NP4260@users.noreply.github.com>
2023-10-19 00:13:34 -0500

    Main Menu Implemented

---
+++ Assets/1 - Scripts/MainMenu_manager.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using Fungus;
+
+public class MainMenu_manager : MonoBehaviour
+{
+    [Header("Buttons")]
+    [SerializeField] private Image playBtn;
+    [SerializeField] private GameObject configBtn;
+    [SerializeField] private GameObject volumeBtn;
+    [SerializeField] private GameObject volumeSlider;
+    [SerializeField] private GameObject retunrConfigBtn;
+    [SerializeField] private GameObject exitBtn;
+    [SerializeField] private GameObject creditsBtn;
+
+
+    [Header("Panels")]
+    [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject configPanel;
+
+    [Header("Game Components")]
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject joystickCanva;
+    [SerializeField] private GameObject buttonsCanva;
+
+    [Header("Others")]
+    [SerializeField] private GameObject gameTitle;
+    [SerializeField] private Image backgroundColor;
+    [SerializeField] private Image screenColor;
+
+    private Color endColor = new Color(185, 185, 185, 0);
+    private Color redColor = new Color(0.1921569f, 0, 0.003921569f, 0);
+    private Color blackColor = new Color(0, 0, 0, 0);
+
+    void Start()
+    {
+        screenColor.gameObject.SetActive(true);
+        screenColor.DOColor(redColor, 6f).OnComplete(() =>
+        {
+            screenColor.gameObject.SetActive(false);
+        });
+
+        Tweener playBtnAnim = playBtn.DOColor(endColor, 2f);
+        playBtnAnim.SetEase(Ease.InOutSine);
+        playBtnAnim.SetLoops(-1,LoopType.Yoyo);
+        playBtnAnim.Play();
+    }
+
+    //public void IntroAnim()
+    //{
+    //    screenColor.enabled = true;
+    //    screenColor.DOColor(redColor, 6f).OnComplete(() =>
+    //    {
+    //        screenColor.gameObject.SetActive(false);
+    //    });
+    //}
+
+    public void OpenConfigAnim()
+    {
+        mainMenuPanel.SetActive(false);
+        configPanel.SetActive(true);
+    }
+
+    public void ExitConfigAnim()
+    {
+        mainMenuPanel.SetActive(true);
+        configPanel.SetActive(false);
+    }
+
+    public void PlayAnim()
+    {
+        mainMenuPanel.transform.DOScale(new Vector3(7, 7, 7), 3).OnComplete(() =>
+        {
+            backgroundColor.DOColor(blackColor, 3).OnComplete(() =>
+            {
+                joystickCanva.gameObject.SetActive(true);
+                buttonsCanva.gameObject.SetActive(true);
+                player.gameObject.SetActive(true);
+
+                gameObject.SetActive(false);
+                DOTween.KillAll(gameObject);
+            });
+
+        });
+    }
+
+
+}
